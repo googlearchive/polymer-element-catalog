fa0eb02ce9fd82a6619d860558fd17224ee8b3ba
Michael Bleigh <mbleigh@mbleigh.com>
2015-05-12 20:36:37 -0400

    Adds gold-elements, fixes detection of multi-dash-elements

--- build/catalog/packages.js
+++ build/catalog/packages.js
@@ -6,25 +6,25 @@ var packageElements = require('./utils/package-elements');
 var parseVersion = require('./utils/parse-version');
 
 module.exports = function (imports) {
-  
+
   var root = imports.root;
   var bowerFile = require(root + '/bower.json');
   var deps = bowerFile.dependencies;
   var guidesStream = imports.guides;
-  
+
   return stream.compose(
     stream.parse('packages.*'),
     stream.filter(function (package) {
-      
+
       return deps[package.name];
     }),
     stream.asyncMap(function (package, done) {
-      
+
       var details = packageDetails({
         root: root,
         name: package.name
       });
-      
+
       var elements = packageElements({
         name: package.name,
         deps: details.dependencies
@@ -33,18 +33,18 @@ module.exports = function (imports) {
       // Parse all guides and add to packages meta
       guidesStream
         .pipe(stream.filter(function (guide) {
-          
+
           return guide.package === package.name
         }))
         .pipe(stream.concat(function (guides) {
-          
+
           package.version = details._release;
           package.description = details.description;
           package.elements = elements;
           package.guides = _.pluck(guides, 'name');
 
           console.log("===",package.name,"(" + details._release + ")");
-          
+
           done(null, package);
         }));
     })
--- build/catalog/utils/package-elements.js
+++ build/catalog/utils/package-elements.js
@@ -1,25 +1,24 @@
 var _ = require('lodash');
 
 module.exports = function (imports) {
-  
+
   var name = imports.name;
   var deps = imports.deps;
-  
+
   deps = deps || {};
-  
+
   var prefix = name.split('-');
-  
   // No prefix, no tags
   if (prefix.length < 2) {
     return [];
   }
-  
+
   return _(deps)
     .keys()
     .filter(function (dep) {
-    
+
       var depPrefix = dep.split('-');
-      return depPrefix.length === 2 && depPrefix[0] === prefix[0];
+      return depPrefix.length >= 2 && depPrefix[0] === prefix[0];
     })
     .value();
 }
