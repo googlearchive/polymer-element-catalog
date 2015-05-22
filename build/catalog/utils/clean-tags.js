ab4d797931277c0454b3b2e310d245e9ffa28c9e
Michael Bleigh <mbleigh@mbleigh.com>
2015-05-22 04:45:39 -0400

    Also scrub product line name tags.

--- build/catalog/utils/clean-tags.js
+++ build/catalog/utils/clean-tags.js
@@ -1,4 +1,4 @@
-var excluded = ['web-components','polymer','web-component'];
+var excluded = ['web-components','polymer','web-component','gold','paper','google','iron'];
 module.exports = function(tag) {
   return excluded.indexOf(tag.toLowerCase()) < 0;
 }
